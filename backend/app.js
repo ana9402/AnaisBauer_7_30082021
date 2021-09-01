1b9835e143594e2caec66e061c6d5a7b0cf15e97
Anaïs <anais75.b@gmail.com>
2021-09-01 17:48:24 +0200

    Initialisation des serveurs front et back

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+// IMPORTATIONS
+const express = require('express');
+const app = express();
+
+// MIDDLEWARES
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a bien été reçue !' });
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !');
+});
+
+// EXPORTATIONS
+module.exports = app;
