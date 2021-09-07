af0a9e2cdd1f8deb23bd513eb906244c01a2caa2
Anaïs <anais75.b@gmail.com>
2021-09-07 12:47:47 +0200

    Màj du header principal avec lien createPost

--- frontend/src/components/MainHeader.vue
+++ frontend/src/components/MainHeader.vue
@@ -1,15 +1,20 @@
 <template>
     <header>
-        <a href="#/home">
-            <img id="logo" src="../assets/icon-left-font.svg" alt="logo Groupomania"/>
-        </a>
+        <div id="top-header">
+            <router-link to="/home">
+                <img id="logo" src="../assets/icon-left-font.svg" alt="logo Groupomania"/>
+            </router-link>
+            <div id="newPost"> 
+                <router-link to="/submit" class="link" id="newPost-link">+<span id="newPost-link_text">Ajouter un post</span></router-link>
+            </div>
+        </div>
         <nav>
             <ul>
                 <li>
-                    <router-link to="/home" id="signup-link">Accueil</router-link>
+                    <router-link to="/home" class="nav-link link"  id="signup-link">Accueil</router-link>
                 </li>
                 <li>
-                    <router-link to="/Profile" id="login-link">Mon Compte</router-link>
+                    <router-link to="/Profile" class="nav-link link"  id="login-link">Mon Compte</router-link>
                 </li>
             </ul>
         </nav>
@@ -32,10 +37,42 @@ header {
     & #logo {
         display: flex;
         width: 100%;
-        height: 50%;
+        height: 100%;
+    }
+    & #top-header {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 20px;
     }
 }
-
+#newPost {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    & #newPost-link {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: rgb(49, 48, 48);
+        color: white;
+        box-shadow: 2px 2px 5px rgb(161, 159, 159);
+        border-radius: 20px;
+        padding: 10px 15px;
+        &_text {
+            align-items: center;
+            justify-content: center;
+            margin-left: 10px;
+        }
+    }
+}
+.link {
+    text-decoration: none;
+    color: black;
+    &:hover {
+        color: #FD2D01;
+    }
+}
 nav {
     & ul {
         list-style-type: none;
@@ -47,53 +84,59 @@ nav {
     }
     
     & a {
-        text-decoration: none;
-        color: black;
         margin-right: 30px;
-        &:hover {
-        color: #FD2D01;
-        }
     }
-
 }
-
-
 
 @media screen and (max-width: 768px) {
     header {
         display: flex;
         flex-direction: column;
         padding: 0;
+        #top-header {
+            width: 100%;
+            justify-content: space-between;
+            padding: 0;
+            margin-right: 30px;
+        }
+    }
+    #newPost {
+        width: 40px;
+    }
+    #newPost-link {
+        width: 100%;
+        margin: 0;
+        border-radius: 20px;
+        &_text {
+            display: none;
+        }
     }
 
     nav {
         width: 100%;
-    }
-
-    nav ul {
-        display: flex;
-        justify-content: center;
-        margin-top: 30px;
-        padding: 0;
-    }
-
-    nav li {
-        display: flex;
-        justify-content: center;
-        padding: 0 0 20px 0;
-        width: 100%;
-        border-bottom: 2px #F2F2F2 solid;
-    }
-
-    nav li:hover {
-        border-bottom: 2px #FD2D01 solid;
+        & ul {
+            display: flex;
+            justify-content: center;
+            margin-top: 30px;
+            padding: 0;
+            & li {
+                display: flex;
+                justify-content: center;
+                padding: 0 0 20px 0;
+                width: 100%;
+                border-bottom: 2px #F2F2F2 solid;
+                &:hover {
+                    border-bottom: 2px #FD2D01 solid;
+                }
+            }
+        }
     }
 
     #signup-link {
         margin: 0;
     }
  
-    router-link {
+    .nav-link {
         width: 100%;
     }
 }
